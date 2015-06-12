e77b0cb47521429ed5e0ed80984ade4ec5a15d6b
Alek Ratzloff <alekratz@gmail.com>
2015-06-12 18:04:32 -0400

    Add iteration of all combinations of N length

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate getopts;
+extern crate char_iter;
 
 mod trtable;
 
@@ -35,6 +36,20 @@ fn permute(index: usize, term: &str, built: String, trtab: &TrTable) {
   }
 }
 
+fn all_combos(chars: &Vec<char>, count: usize, built: String) {
+  // base case
+  if count == 0 {
+    println!("{}", built);
+    return;
+  }
+
+  for letter in chars {
+    let mut new_built: String = (&built).to_string();
+    new_built.push(*letter);
+    all_combos(&chars, count - 1, new_built);
+  }
+}
+
 fn print_usage(program: &str, opts: Options) {
   let brief = format!("Usage: {} [options]", program);
   print!("{}", opts.usage(&brief));
@@ -45,6 +60,12 @@ fn main() {
   let program = args[0].clone();
 
   let mut opts = Options::new();
+  opts.optopt("c", "combos", "iterate through all possible printable combinations of N letters. You may specify a comma separated list, e.g. 1,2,4,5", 
+    "N");
+  opts.optflag("", "no-upper", "specific to -c. If this flag is set, all combinations will omit upper case letters.");
+  opts.optflag("", "no-lower", "specific to -c. If this flag is set, all combinations will omit lower case letters.");
+  opts.optflag("", "no-numbers", "specific to -c. If this flag is set, all combinations will omit numbers.");
+  opts.optflag("", "no-symbols", "specific to -c. If this flag is set, all combinations will omit symbols.");
   opts.optopt("p", "permute", "permute over a term with similar letters. Use the -t option to specify a table file", 
     "TERM");
   opts.optopt("t", "trtab", "translation table file, in YAML format. Required with the -p option.", "FILENAME");
@@ -73,6 +94,53 @@ fn main() {
         permute(0, &permute_str, "".to_string(), &trtab);
       },
       None => println!("You must specify a table file with the permute option")
+    }
+  // all combinations 
+  } else if let Some(combos) = matches.opt_str("c") {
+    let counts = combos.split(",")
+      .map(|n| n.parse::<usize>().unwrap());
+
+    let mut all_chars = Vec::new();
+    // upper case letters
+    if !matches.opt_present("no-upper") {
+      for l in char_iter::new('\x41', '\x5a') {
+        all_chars.push(l);
+      }
+    }
+    // lower case letters
+    if !matches.opt_present("no-lower") {
+      for l in char_iter::new('\x61', '\x7a') {
+        all_chars.push(l);
+      }
+    }
+    // numbers
+    if !matches.opt_present("no-numbers") {
+      for l in char_iter::new('\x30', '\x39') {
+        all_chars.push(l);
+      }
+    }
+    // symbols
+    if !matches.opt_present("no-symbols") {
+      // <space> to /
+      for l in char_iter::new('\x20', '\x2F') {
+        all_chars.push(l);
+      }
+      // : to @
+      for l in char_iter::new('\x3a', '\x40') {
+        all_chars.push(l);
+      }
+      // [ to `
+      for l in char_iter::new('\x5b', '\x60') {
+        all_chars.push(l);
+      }
+      // { to ~
+      for l in char_iter::new('\x7b', '\x7e') {
+        all_chars.push(l);
+      }
+    }
+
+    for n in counts {
+      all_combos(&all_chars, n, String::new());
     }
   }
 }
